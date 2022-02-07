60359d65d245eb5089bccf1e50fe67ae7a40c773
Kristaps Dzerve-Strals <kristaps@kriss.tech>
2022-02-07 16:35:08 +0200

    Add premium calculation logic

--- src/main/java/proofit/insurance/calculator/services/PremiumCalculator.java
+++ src/main/java/proofit/insurance/calculator/services/PremiumCalculator.java
@@ -1,11 +1,61 @@
 package proofit.insurance.calculator.services;
 
 import java.math.BigDecimal;
+import java.math.RoundingMode;
+
 import proofit.insurance.calculator.domain.Policy;
+import proofit.insurance.calculator.domain.RiskType;
 
 public class PremiumCalculator {
 
     public BigDecimal calculate(Policy policy) {
-        throw new UnsupportedOperationException();
+        var firePremium = calculatePremiumForRiskType(policy, RiskType.FIRE);
+        var theftPremium = calculatePremiumForRiskType(policy, RiskType.THEFT);
+        var insurancePremium = firePremium.add(theftPremium);
+
+        // return two decimal places as that is what's requested in the acceptance
+        // criteria
+        return insurancePremium.setScale(2, RoundingMode.HALF_UP);
+    }
+
+    private BigDecimal calculatePremiumForRiskType(Policy policy, RiskType riskType) {
+        var sumInsured = getInsuranceSum(policy, riskType);
+        var coefficient = getPremiumCoefficient(sumInsured, riskType);
+        return sumInsured.multiply(coefficient);
+    }
+
+    private BigDecimal getInsuranceSum(Policy policy, RiskType riskType) {
+        return policy.getPolicyObjects()
+                .stream()
+                .map(policyObject -> {
+                    var sumInsured = policyObject.getSubObjects()
+                            .stream()
+                            .filter(subObject -> {
+                                return subObject.getRiskType() == riskType;
+                            })
+                            .map(subObject -> subObject.getSumInsured())
+                            .reduce(BigDecimal.ZERO, BigDecimal::add);
+                    return sumInsured;
+                })
+                .reduce(BigDecimal.ZERO, BigDecimal::add);
+    }
+
+    private BigDecimal getPremiumCoefficient(BigDecimal insuranceSum, RiskType riskType) {
+        switch (riskType) {
+            case FIRE:
+                if (insuranceSum.compareTo(new BigDecimal(100)) > 0) {
+                    return BigDecimal.valueOf(0.024);
+                }
+
+                return BigDecimal.valueOf(0.014);
+            case THEFT:
+                if (insuranceSum.compareTo(new BigDecimal(15)) > 0) {
+                    return BigDecimal.valueOf(0.05);
+                }
+
+                return BigDecimal.valueOf(0.11);
+            default:
+                throw new IllegalStateException(String.format("Unsupported risk type: %s", riskType));
+        }
     }
 }
--- src/test/java/proofit/insurance/calculator/services/PremiumCalculatorTests.java
+++ src/test/java/proofit/insurance/calculator/services/PremiumCalculatorTests.java
@@ -22,6 +22,14 @@ public class PremiumCalculatorTests {
         this.calculator = new PremiumCalculator();
     }
 
+    /**
+     * Test case for the first acceptance criteria:
+     * If there is one policy, one object and two sub-objects as described below,
+     * then calculator should return
+     * premium = 2.28 EUR
+     * Risk type = FIRE, Sum insured = 100.00
+     * Risk type = THEFT, Sum insured = 8.00
+     */
     @Test
     void Test_CanCalculatePremium_OneObjectTwoSubObjects() {
         var policy = new Policy(
@@ -51,7 +59,82 @@ public class PremiumCalculatorTests {
 
         var result = this.calculator.calculate(policy);
 
-        var expected = new BigDecimal(2.28);
+        var expected = BigDecimal.valueOf(2.28);
+        assertEquals(expected, result);
+    }
+
+    /**
+     * Test case for the second acceptance criteria:
+     * If policy's total sum insured for risk type FIRE and total sum insured for
+     * risk type THEFT are as
+     * described below, then calculator should return premium = 17.13 EUR
+     * Risk type = FIRE, Sum insured = 500.00
+     * Risk type = THEFT, Sum insured = 102.51
+     */
+    @Test
+    void Test_CanCalculatePremium_SumBasedTest() {
+        var policy = new Policy(
+                "TEST_POLICY_NUMBER",
+                PolicyStatus.APPROVED,
+                new ArrayList<PolicyObject>() {
+                    {
+                        add(
+                                new PolicyObject(
+                                        "Home",
+                                        new ArrayList<PolicySubObject>() {
+                                            {
+                                                add(
+                                                        new PolicySubObject(
+                                                                "TV",
+                                                                new BigDecimal(200.00),
+                                                                RiskType.FIRE));
+                                                add(
+                                                        new PolicySubObject(
+                                                                "PC",
+                                                                new BigDecimal(50.00),
+                                                                RiskType.THEFT));
+                                            }
+                                        }));
+                        add(
+                                new PolicyObject(
+                                        "Work",
+                                        new ArrayList<PolicySubObject>() {
+                                            {
+                                                add(
+                                                        new PolicySubObject(
+                                                                "TV",
+                                                                new BigDecimal(200.00),
+                                                                RiskType.FIRE));
+                                                add(
+                                                        new PolicySubObject(
+                                                                "PC",
+                                                                new BigDecimal(50.00),
+                                                                RiskType.THEFT));
+                                            }
+                                        }));
+                        add(
+                                new PolicyObject(
+                                        "Some Thid Object",
+                                        new ArrayList<PolicySubObject>() {
+                                            {
+                                                add(
+                                                        new PolicySubObject(
+                                                                "TV",
+                                                                new BigDecimal(100.00),
+                                                                RiskType.FIRE));
+                                                add(
+                                                        new PolicySubObject(
+                                                                "PC",
+                                                                new BigDecimal(2.51),
+                                                                RiskType.THEFT));
+                                            }
+                                        }));
+                    }
+                });
+
+        var result = this.calculator.calculate(policy);
+
+        var expected = BigDecimal.valueOf(17.13);
         assertEquals(expected, result);
     }
 }
